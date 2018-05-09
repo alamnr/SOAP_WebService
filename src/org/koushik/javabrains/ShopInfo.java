fe3cd4c72fe9f88665bcac6ced672b8ba0c6d499
alamnr <alamnr@gmail.com>
2018-05-09 16:36:43 +0600

    Commoit for Soap Web Service-12-Schema_Types_and_Binding_Styles

---
+++ src/org/koushik/javabrains/ShopInfo.java
@@ -0,0 +1,33 @@
+package org.koushik.javabrains;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+
+
+
+@WebService
+// When do you use document  and when do you use rpc style, though document is default
+//@SOAPBinding(style=Style.RPC) // When RPC style is used , no schema gets generated for types
+@SOAPBinding(style=Style.DOCUMENT) // use document for complex schema type
+public class ShopInfo {
+	
+	@WebMethod
+	@WebResult(partName="lookupOutPut")
+	public String getShopInfo(@WebParam(partName="lookupInput") String property){
+		String response = "Invalid property";
+		
+		if("shopName".equals(property)){
+			response = "Test Mart";
+		}
+		else if("since".equals(property)){
+			response ="since 2012";
+		}
+		
+		return response;
+	}
+
+}
